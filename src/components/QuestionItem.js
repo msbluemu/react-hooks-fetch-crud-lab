61c647d645e05aa93fd1c858473405975534ec39
Qianyun Mu <msbluemu@gmail.com>
2023-07-16 15:37:30 +1000

    complete lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,41 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion}) {
   const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+  let options = [];
+  if (answers && typeof answers === "object") {
+    options = Object.values(answers).map((answer, index) => (
+      <option key={index} value={index}>
+        {answer}
+      </option>
+    ));
+  }
+
+  function handleDeleteClick(){
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "DELETE",
+    })
+    .then((r) => r.json())
+    .then(() => onDeleteQuestion(question))
+  }
+
+ 
+  function handleAnswerChange(event) {
+    const newCorrectIndex = parseInt(event.target.value);
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: newCorrectIndex,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedQuestion) => onUpdateQuestion(updatedQuestion));
+  }
 
   return (
     <li>
@@ -15,11 +43,17 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select
+          defaultValue={correctIndex}
+          onChange={handleAnswerChange}
+        >
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
+
 
 export default QuestionItem;
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+
+   function handleDeleteQuestion(deletedQuestion){
+    const updatedQuestions = questions.filter((question) => question.id !== deletedQuestion.id);
+    setQuestions(updatedQuestions);
+   }
+
+  function handleUpdateQuestion(updatedQuestion){
+    const updatedQuestionAnswers = questions.map((question) => {
+      if(question.id === updatedQuestion.id){
+        return updatedQuestion;
+      } else {
+        return question;
+      }
+    });
+    setQuestions(updatedQuestionAnswers)
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((data) => {
+        setQuestions(data)
+      });
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{
+          questions.map((question) => (
+         <QuestionItem 
+          key={question.id} 
+          question={question} 
+          onDeleteQuestion={handleDeleteQuestion}
+          onUpdateQuestion = {handleUpdateQuestion}
+          />
+        ))}
+    </ul>
     </section>
   );
 }
